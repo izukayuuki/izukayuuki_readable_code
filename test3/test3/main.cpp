45616c813ad49af4eb2cd33d211e9b8dbb6d2121
izukayuuki <izukayuuki1030@gmail.com>
2021-09-13 18:43:07 +0900

    仕様１

---
+++ test3/test3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  test3
+//
+//  Created by 井塚雄喜 on 2021/09/13.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
