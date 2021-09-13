f2df4e2dfac2880625d0ebbe5aa66ef0ae4caa3b
izukayuuki <izukayuuki1030@gmail.com>
2021-09-13 16:03:02 +0900

    仕様１

---
+++ test1/test1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  test1
+//
+//  Created by 井塚雄喜 on 2021/09/13.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "上手\n";
+    return 0;
+}
